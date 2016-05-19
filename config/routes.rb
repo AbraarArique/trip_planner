2e6481d169b849c8d17613ed12db855f406e8b5c
Abraar Arique <abraar@abraararique.com>
2016-05-19 07:48:56 +0600

    Add password reset, change default layout.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+  get 'users/password_reset', to: 'trips#password_reset', as: 'password_reset'
+  post 'users/recover', to: 'trips#recover', as: 'recover'
   root 'trips#index'
-  resources :trips, only: [:index, :show, :destroy]
-  resources :events, except: :index
+  resources :trips do
+    resources :events, except: :index
+  end
   resources :pin_notes, except: [:index, :show]
 end
